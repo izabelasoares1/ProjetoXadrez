c09b3b839aaf200d7bdb11afe0aa6610340e9e66
Izabela Soares <izabelasoares001@gmail.com>
2017-11-29 10:28:35 -0200

    Criando a classe Posicao

--- projeto-final-part1/Program.cs
+++ projeto-final-part1/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-
+using Tabuleiro;
 
 namespace projeto_final_part1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Olá");
+
+            Posicao P;
+
+            P = new Tabuleiro.Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
             Console.ReadLine();
         }
     }
